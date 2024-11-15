5025bca5180d1ced41c2e9ccbc68c6a96476afab
XCai777 <xiorence_cai777@yahoo.com>
2024-11-15 13:56:24 +0800

    Update truckkun.py

--- TruckKun/truckkun.py
+++ TruckKun/truckkun.py
@@ -28,6 +28,7 @@ warnings.filterwarnings("ignore")
 
 st.set_page_config(page_title="Truck-Kun, your partner in delivery", page_icon="🚚", layout="wide")
 
+@st.cache_data(ttl=60)
 def load_data():
     try:
         return pd.read_csv('https://raw.githubusercontent.com/XCai777/AI_Republic_Bootcamp/refs/heads/main/TruckKun/truckkun.csv')
